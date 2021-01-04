5ec627b8ce82effd097c65eab4869f768df1da3a
pavlo.v <pavlo.v@kakaocorp.com>
2021-01-04 18:16:59 +0900

    feat($Car) : add racing method and refactoring
    - add public method `racing()` in Car.java
    - refactoring `goFront()` method

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,2 +1,22 @@
-package racingcar;public class Car {
+package racingcar;
+
+public class Car {
+
+    private int position;
+    public static final int STEP = 1;
+
+    public void goFront() {
+        position += STEP;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public int racing(int randomNum) {
+        if (randomNum >= 4) {
+            goFront();
+        }
+        return getPosition();
+    }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,4 +1,27 @@
-import static org.junit.jupiter.api.Assertions.*;
+package racingcar;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class CarTest {
-  
+    Car car;
+    @BeforeEach
+    void setUp() {
+        car = new Car();
+    }
+
+    @Test
+    void goFront() {
+        int prePosition = car.getPosition();
+        assertThat(car.racing(4)).isEqualTo(prePosition + Car.STEP);
+    }
+
+    @Test
+    void stop() {
+        int prePosition = car.getPosition();
+        assertThat(car.racing(3)).isEqualTo(prePosition);
+    }
+
 }
