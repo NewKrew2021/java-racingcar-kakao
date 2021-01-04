58e96f0786391d7989b3a5c36daf6c1196a9319c
danny.ho <danny.ho@kakaocorp.com>
2021-01-04 18:18:45 +0900

    feat: implement function to move forward a car

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,20 +1,36 @@
 public class Car {
     private String name;
+    private int position;
 
     public Car(String name) {
-        if(name == null){
+        if (name == null) {
             throw new IllegalArgumentException();
         }
-        if(name.length() == 0){
+        if (name.length() == 0) {
             throw new IllegalArgumentException();
         }
-        if(name.length() >= 6) {
+        if (name.length() >= 6) {
             throw new IllegalArgumentException();
         }
         this.name = name;
+        this.position = 0;
+    }
+
+    public void moveOrNot(int condition) {
+        if (condition >= 4) {
+            moveForwardOne();
+        }
+    }
+
+    private void moveForwardOne() {
+        this.position++;
     }
 
     public String getName() {
-        return name;
+        return this.name;
+    }
+
+    public int getPosition() {
+        return this.position;
     }
 }
