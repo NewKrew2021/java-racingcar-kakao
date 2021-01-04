d1d62dc92d3a837306bedc74f28e61db3fcc0c58
dddfff22 <dddfff22@naver.com>
2021-01-04 15:11:15 +0900

    feat: 숫자 하나를 입력하면 해당 숫자를 반환

--- src/main/java/Calculate/Calculate.java
+++ src/main/java/Calculate/Calculate.java
@@ -5,6 +5,6 @@ public class Calculate {
         if(formula==null||formula.isEmpty()){
             return 0;
         }
-        return 1;
+        return Integer.parseInt(formula);
     }
 }
--- src/test/java/Calculate/CalculateTest.java
+++ src/test/java/Calculate/CalculateTest.java
@@ -1,14 +1,20 @@
 package Calculate;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculateTest {
+    private Calculate calculate;
+    @BeforeEach
+    void setUp() {
+         calculate = new Calculate();
+    }
 
     @Test
     public void empty_String(){
-        Calculate calculate=new Calculate();
         assertEquals(0,calculate.parser(""));
         assertEquals(0,calculate.parser(""));
 
@@ -16,8 +22,12 @@ public class CalculateTest {
 
     @Test
     public void null_String(){
-        Calculate calculate=new Calculate();
         assertEquals(0,calculate.parser(null));
 
+    }
+
+    @Test
+    public void singleNumber() {
+        Assertions.assertThat(calculate.parser("1")).isEqualTo(1);
     }
 }
