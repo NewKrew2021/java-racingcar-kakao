35ec0fde7abf8f411f3f2827c25a23f973177b96
YunGoon <dsyun96@gmail.com>
2021-01-04 17:12:18 +0900

    차의 전진 기능

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,11 +1,21 @@
 public class Car {
     private final String name;
+    private int position;
 
     public Car(String name) {
         this.name = name;
+        this.position = 0;
     }
 
     public boolean isValid() {
         return name.length() <= 5;
+    }
+
+    public void go() {
+        this.position++;
+    }
+
+    public int getPosition() {
+        return this.position;
     }
 }
--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -8,4 +8,13 @@ public class CarTest {
         assertThat(new Car("snoopy").isValid()).isFalse();
         assertThat(new Car("yuni").isValid()).isTrue();
     }
+
+    @Test
+    public void go() {
+        Car car = new Car("jack");
+        car.go();
+        car.go();
+        car.go();
+        assertThat(car.getPosition()).isEqualTo(3);
+    }
 }
