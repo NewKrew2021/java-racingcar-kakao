62caecc72d4090c413d62e9617926fd97951617c
brody <youoper@gmail.com>
2021-01-05 18:22:59 +0900

    refactor(CarTest): 테스트마다 Car 객체를 초기화하기 위한 BeforeEach 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,18 +1,22 @@
 package racingcar;
 
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
 
+    private Car car;
+
+    @BeforeEach
+    void initializeCar() {
+        this.car = new Car("", 0);
+    }
+
     @Test
     void getDistanceTest(){
-        Car car = new Car("", 0);
         int stopRandomNumber = 3;
         int goRandomNumber = 4;
         assertThat(car.getDistance(stopRandomNumber)).isEqualTo(0);
@@ -21,13 +25,11 @@ public class CarTest {
 
     @Test
     void makeRandomNumberTest(){
-        Car car = new Car("", 0);
         assertThat(car.makeRandomNumber()).isGreaterThan(-1).isLessThan(10);
     }
 
     @Test
     void updateCarPositionTest(){
-        Car car = new Car("", 0);
         int stopDistance = 0;
         int goDistance = 1;
         assertThat(car.updateCarPosition(stopDistance)).isEqualTo(0);
