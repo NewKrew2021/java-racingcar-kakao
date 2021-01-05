590ecbaa07b1b10f2022244fce7ad7c6ac52d498
YunGoon <dsyun96@gmail.com>
2021-01-05 10:43:37 +0900

    게임 종료 여부 테스트 및 main 구현

--- src/main/java/RacingGame.java
+++ src/main/java/RacingGame.java
@@ -1,21 +1,51 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class RacingGame {
-    private final int roundNo;
+    private final int ROUND_NO;
     private final List<Car> cars;
+    private static final int MAX_LENGTH = 5;
+    private int curRound;
 
     public RacingGame(int roundNo, List<Car> cars) {
-        this.roundNo = roundNo;
+        this.ROUND_NO = roundNo;
         this.cars = cars;
+        this.curRound = 0;
     }
 
-    public List<Car> getWinner() {
+    public List<Car> getWinners() {
         int maxPosition = getMaxPosition();
 
         return cars.stream()
                 .filter(car -> car.getPosition() == maxPosition)
                 .collect(Collectors.toList());
+    }
+
+    public void printWinners() {
+        List<Car> winners = getWinners();
+        String[] winnersName = new String[winners.size()];
+
+        for (int i = 0; i < winnersName.length; ++i) {
+            winnersName[i] = winners.get(i).getName();
+        }
+
+        System.out.println(String.join(", ", winnersName) + "가 최종 우승했습니다.");
+    }
+
+    public boolean isEnd() {
+        return curRound == ROUND_NO;
+    }
+
+    public void playRound() {
+        MyRandom random = new MyRandom();
+        for (Car car : cars) {
+            car.run(random.nextInt());
+            car.print();
+        }
+        curRound++;
     }
 
     private int getMaxPosition() {
@@ -24,5 +54,39 @@ public class RacingGame {
             maxPosition = Math.max(maxPosition, car.getPosition());
         }
         return maxPosition;
+    }
+
+    private static boolean isValidNames(String[] names) {
+        List<String> nameList = Arrays.asList(names);
+        return nameList.stream()
+                .noneMatch(name -> name.length() > MAX_LENGTH);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] carNames;
+
+        do {
+            System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+            carNames = sc.nextLine().split(",");
+        } while (!isValidNames(carNames));
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int roundNo = sc.nextInt();
+
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+
+        RacingGame game = new RacingGame(roundNo, cars);
+
+        System.out.println("실행 결과");
+        while (!game.isEnd()) {
+            game.playRound();
+            System.out.println();
+        }
+
+        game.printWinners();
     }
 }
--- src/test/java/RacingGameTest.java
+++ src/test/java/RacingGameTest.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class RacingGameTest {
     @Test
-    public void winner() {
+    public void winners() {
         Car c1 = new Car("car1");
         Car c2 = new Car("car2");
         Car c3 = new Car("car3");
@@ -17,6 +17,19 @@ public class RacingGameTest {
         c2.go();
         c3.go();
         c3.go();
-        assertThat(game.getWinner().equals(Arrays.asList(c2, c3))).isTrue();
+        assertThat(game.getWinners().equals(Arrays.asList(c2, c3))).isTrue();
+    }
+
+    @Test
+    public void isGameEnd() {
+        Car c1 = new Car("car1");
+        Car c2 = new Car("car2");
+        Car c3 = new Car("car3");
+        RacingGame game = new RacingGame(3, Arrays.asList(c1, c2, c3));
+
+        game.playRound();
+        game.playRound();
+        game.playRound();
+        assertThat(game.isEnd()).isTrue();
     }
 }
