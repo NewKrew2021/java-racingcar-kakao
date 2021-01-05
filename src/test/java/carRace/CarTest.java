e404f156eb5c72a56c64717216470763e7ee82b7
sierra.s <sierra.s@kakaocorp.com>
2021-01-05 21:15:57 +0900

    refactor(CarTest class) : 테스트에 사용되는 숫자 상수화

--- src/test/java/carRace/CarTest.java
+++ src/test/java/carRace/CarTest.java
@@ -6,12 +6,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
 
+    private final int MOVE = 4;
+    private final int NOT_MOVE = 3;
+
     @Test
-    public void toStringTest() {
+    public void judgeAndGoTest() {
         Car testCar = new Car("test");
-        testCar.judgeAndGo(5);
-        testCar.judgeAndGo(6);
-        testCar.judgeAndGo(1);
+        testCar.judgeAndGo(MOVE);
+        testCar.judgeAndGo(MOVE);
+        testCar.judgeAndGo(NOT_MOVE);
         assertThat(testCar.toString()).isEqualTo("test : --");
     }
 
@@ -19,9 +22,9 @@ public class CarTest {
     public void compareToTest() {
         Car testCar1 = new Car("test1");
         Car testCar2 = new Car("test2");
-        testCar1.judgeAndGo(5);
-        testCar2.judgeAndGo(5);
-        testCar2.judgeAndGo(5);
+        testCar1.judgeAndGo(MOVE);
+        testCar2.judgeAndGo(MOVE);
+        testCar2.judgeAndGo(MOVE);
         assertThat(testCar1.compareTo(testCar2)).isGreaterThan(0);
     }
 }
