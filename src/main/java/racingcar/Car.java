9aa2a2bd6a1a6bb0905f36c6fc546dd7fcc51e3e
brody <youoper@gmail.com>
2021-01-05 13:21:29 +0900

    feat(Car): 자동차 클래스 구현 및 난수값에 따른 0,1을 분류하는 함수 구현

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class Car {
+    public int getDistance(int randomNumber){
+        if(randomNumber<4){
+            return 0;
+        }
+        return 1;
+    }
+
+}
