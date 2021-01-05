0932bc72de6aac501d154f9612ab807c3c41a03c
Jjungs7 <jeongy95@gmail.com>
2021-01-05 15:49:13 +0900

    feat(simulator): simulator to run racingcar

---
+++ src/main/java/com/nextstep/racingcar/Main.java
@@ -0,0 +1,9 @@
+package com.nextstep.racingcar;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Simulator simulator = new Simulator();
+    simulator.play();
+  }
+}
---
+++ src/main/java/com/nextstep/racingcar/Simulator.java
@@ -0,0 +1,44 @@
+package com.nextstep.racingcar;
+
+import com.nextstep.racingcar.racingcar.Racingcar;
+import com.nextstep.racingcar.ui.UserInput;
+
+import static com.nextstep.racingcar.ui.MessagePrinter.*;
+
+public class Simulator {
+
+  private UserInput inp;
+  private Racingcar racingcar;
+  private String[] carNames;
+  private int repeatCount;
+
+  public void play() {
+    initializeUI();
+    getInputs();
+    createNewRacingcarBasedOnInputs();
+
+    printResultGuide();
+    while (!racingcar.isFinished()) {
+      racingcar.simulateOnce();
+      printSimulationResults(racingcar);
+    }
+
+    printWinners(racingcar.getWinners());
+  }
+
+  private void initializeUI() {
+    inp = new UserInput();
+  }
+
+  private void getInputs() {
+    printRequestCarNames();
+    carNames = inp.getCarNames();
+
+    printRequestRepeatCount();
+    repeatCount = inp.getRepeatCount();
+  }
+
+  private void createNewRacingcarBasedOnInputs() {
+    racingcar = new Racingcar(carNames, repeatCount);
+  }
+}
