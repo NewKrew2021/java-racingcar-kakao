c43dfc3b7175bd82428fd591435e696ede95b143
Jjungs7 <jeongy95@gmail.com>
2021-01-05 15:19:50 +0900

    refactor(inputcheck): merge two functions to one
    two functions in one regex make clearer code

--- src/main/java/com/nextstep/racingcar/utils/InputCheck.java
+++ src/main/java/com/nextstep/racingcar/utils/InputCheck.java
@@ -3,21 +3,13 @@ package com.nextstep.racingcar.utils;
 public class InputCheck {
 
   public static boolean isValidName(String input) {
-      if (!isAllowedInput(input)) {
-          throw new RuntimeException();
-      }
+    if (!isAllowedInput(input)) {
+      throw new RuntimeException();
+    }
     return true;
   }
 
   private static boolean isAllowedInput(String input) {
-    return !isOverMaxLength(input) && isAlpha(input);
-  }
-
-  private static boolean isOverMaxLength(String input) {
-    return input.length() > 5;
-  }
-
-  private static boolean isAlpha(String input) {
-    return input.matches("[a-zA-Z]+");
+    return input.matches("[a-zA-Z]{1,5}");
   }
 }
