da418a4ac4289b01e58958ebd563b8abecf0efe3
Ye-Ji-Kim <kyeoji0128@naver.com>
2021-01-05 11:37:39 +0900

    feat: 자동차의 전진 여부 결정 및 이동 기능

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,8 +2,23 @@ package racingcar;
 
 public class Car {
     private String name;
+    private int location;
 
     public void setName(String name){
         this.name = name;
+    }
+
+    public void move(){
+        if(isMoveFoward(getRandomNo()))
+            this.location++;
+    }
+
+    private boolean isMoveFoward(int number){
+        if(number>=4)
+            return true;
+        return false;
+    }
+    private int getRandomNo(){
+        return (int) (Math.random()*10);
     }
 }
