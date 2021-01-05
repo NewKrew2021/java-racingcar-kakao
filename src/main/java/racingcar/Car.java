26d0c1bc8ef46bfb8cfe5ebc40140940ad2fe6f1
brody <youoper@gmail.com>
2021-01-05 13:28:55 +0900

    feat(Car): 이동여부에 따라 자동차의 위치를 갱신하는 메서드 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,11 +1,16 @@
 package racingcar;
 
 public class Car {
+    private int position = 0;
     public int getDistance(int randomNumber){
         if(randomNumber<4){
             return 0;
         }
         return 1;
+    }
+    public int updateCarPosition(int distance){
+        position += distance;
+        return position;
     }
 
 }
