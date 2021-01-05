62a6fc04dc160f06752ea1fe58483f4a6e19a6f8
tttuer <tttuer1@naver.com>
2021-01-05 13:18:14 +0900

    Feat: update Car, CarTest
    update Car, CarTest

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -6,4 +6,18 @@ public class Car {
             throw new IllegalArgumentException("길이 5 초과");
         }
     }
+
+    public boolean isForword(int randomNumber) {
+        return randomNumber >= MIN_FORWARD_RANDOM;
+    }
+
+    public void tryForward(int randomResult) {
+        if(isForword(randomResult)) {
+            this.location += 1;
+        }
+    }
+
+    public int getLocation() {
+        return location;
+    }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,7 +1,10 @@
 package racingcar;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 public class CarTest {
@@ -9,5 +12,21 @@ public class CarTest {
     void carNameCheckTest() {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> Car.validateName("jaasdf")).withMessageMatching("길이 5 초과");
+    }
+
+    @ParameterizedTest
+    @CsvSource({"3,0","4,1"})
+    void tryForwardTest(int randomResult, int nextLocation){
+        Car car = new Car();
+        car.tryForward(randomResult);
+        assertThat(car.getLocation()).isEqualTo(nextLocation);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"5,true","4,true","3,false","2,false"})
+    void isForwordTest(int randomNumber, boolean result) {
+        Car car = new Car();
+
+        assertThat(car.isForword(randomNumber)).isEqualTo(result);
     }
 }
