a28d437212e493929012d346f0d84dad9ff5edc4
brody <youoper@gmail.com>
2021-01-05 17:45:08 +0900

    feat(CarView): 사용자의 입력을 받고 메시지를 출력하는 CarView 클래스 기능 추가

---
+++ src/main/java/racingcar/CarView.java
@@ -0,0 +1,53 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class CarView {
+
+    public static ArrayList<String> parseCarName(String text){
+        return new ArrayList<String>(Arrays.asList(text.split(",")));
+    }
+
+    public static void validateCarName(ArrayList<String> nameList) throws IllegalStateException {
+        for (String name : nameList) {
+            testLength(name);
+        }
+    }
+
+    private static void testLength(String name){
+        if(name.length() > 5) {
+            throw new IllegalStateException();
+        }
+    }
+
+    public static String writeCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public static int writeRacingRound() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    public static void printWinnerCar(ArrayList<String> winners) {
+        System.out.println(String.join(",", winners) + "가 최종 우승했습니다.");
+    }
+
+    public static void printRoundStart() {
+        System.out.println();
+        System.out.println("실행 결과");
+    }
+
+    public static void printRoundResult(CarGame game) {
+        for(Car car : game.getCarList()){
+            System.out.println(car.getName() + " : " + String.join("", Collections.nCopies(car.getPosition(), "-")));
+        }
+        System.out.println();
+    }
+}
