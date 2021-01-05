c45d00ec0880fbd779b9831fa045bb9acbe55bdd
brody <youoper@gmail.com>
2021-01-05 16:52:26 +0900

    refactor(Car): Car 클래스 생성자 변경

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,6 +3,17 @@ package racingcar;
 public class Car {
     private int position = 0;
     private final int RANDOM_SCOPE = 10;
+    private String name = "";
+
+    public Car(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public int getPosition(){
+        return position;
+    }
+
     public int makeRandomNumber(){
         return ((int)(Math.random() * 10000) % RANDOM_SCOPE);
     }
@@ -19,4 +30,14 @@ public class Car {
         return position;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getNameIfMatched(int position) {
+        if(this.position == position){
+            return this.name;
+        }
+        return "";
+    }
 }
