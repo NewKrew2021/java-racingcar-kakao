03260c1984cc7de8ba9d8af1b9600b597b1a9b51
Jjungs7 <jeongy95@gmail.com>
2021-01-05 17:28:11 +0900

    test(racingcar): Add for isInProgress method

--- src/test/java/com/nextstep/racingcar/racingcar/RacingcarTest.java
+++ src/test/java/com/nextstep/racingcar/racingcar/RacingcarTest.java
@@ -2,35 +2,44 @@ package com.nextstep.racingcar.racingcar;
 
 import org.junit.jupiter.api.Test;
 import java.util.Arrays;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class RacingcarTest {
 
   private final int MOVE = 4;
+  private final int REPEAT_COUNT = 3;
   private final int STOP = 0;
+  private final String[] CAR_NAMES = new String[]{"one", "two", "three"};
+  private final Racingcar RACINGCAR = new Racingcar(CAR_NAMES, REPEAT_COUNT);
 
   @Test
   void oneCarWins() {
-    String[] carNames = new String[]{"losea", "loseb", "win"};
-    Racingcar racingcar = new Racingcar(carNames, 3);
-
-    while (racingcar.isInProgress()) {
-      racingcar.simulate(Arrays.asList(STOP, STOP, MOVE));
+    while (RACINGCAR.isInProgress()) {
+      RACINGCAR.simulate(Arrays.asList(STOP, STOP, MOVE));
     }
 
-    assertThat(racingcar.getWinners()).isEqualTo(Arrays.asList("win"));
+    assertThat(RACINGCAR.getWinners()).isEqualTo(Arrays.asList("three"));
   }
 
   @Test
   void twoCarWins() {
-    String[] carNames = new String[]{"lose", "wina", "winb"};
-    Racingcar racingcar = new Racingcar(carNames, 3);
-
-    while (racingcar.isInProgress()) {
-      racingcar.simulate(Arrays.asList(STOP, MOVE, MOVE));
+    while (RACINGCAR.isInProgress()) {
+      RACINGCAR.simulate(Arrays.asList(STOP, MOVE, MOVE));
     }
 
-    assertThat(racingcar.getWinners()).isEqualTo(Arrays.asList("wina", "winb"));
+    assertThat(RACINGCAR.getWinners()).isEqualTo(Arrays.asList("two", "three"));
+  }
+
+  @ParameterizedTest
+  @CsvSource({"2,true", "3,false"})
+  void checkFinished(int simulateCount, boolean expected) {
+    for (int i = 0; i < simulateCount; i++) {
+      RACINGCAR.simulate(Arrays.asList(STOP, STOP, STOP));
+    }
+
+    assertThat(RACINGCAR.isInProgress()).isEqualTo(expected);
   }
 }
