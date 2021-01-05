14d63e84a8a56e976ee0967eb6543d49876c9004
danny.ho <danny.ho@kakaocorp.com>
2021-01-05 14:14:03 +0900

    feat: add names input UI

---
+++ src/test/java/RacingCarGameTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RacingCarGameTest {
+
+    @Test
+    void racingCarGame(){
+
+
+    }
+}
