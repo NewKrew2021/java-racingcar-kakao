3968451eb317ca95b2ffa055c35717212e60b1d4
corby-kim <corby.kim@kakaocorp.com>
2021-01-06 00:33:06 +0900

    feat: add RacingGame domain

--- src/main/java/RacingCarGame.java
+++ src/main/java/RacingCarGame.java
@@ -1,26 +1,15 @@
-import domain.Cars;
+import domain.RacingGame;
 import domain.RandomEngine;
 import view.InputView;
 import view.OutputView;
 
 public class RacingCarGame {
     public static void main(String[] args) {
-        Cars cars = createCars();
-
-        moveForRounds(cars);
-
-        OutputView.printWinnerCars(cars.findWinnerCars());
-    }
-
-    private static Cars createCars() {
-        return Cars.create(new RandomEngine(), InputView.readNames());
-    }
-
-    private static void moveForRounds(Cars cars) {
-        int round = InputView.readRound();
-        for (int i = 0; i < round; i++) {
-            cars.moveCars();
-            OutputView.printCars(cars.getCars());
+        RacingGame game = RacingGame.initialize(new RandomEngine(), InputView.readNames(), InputView.readRound());
+        while (!game.isEnd()) {
+            game.moveCars();
+            OutputView.printCars(game.getCars());
         }
+        OutputView.printWinnerCars(game.findWinnerCars());
     }
 }
