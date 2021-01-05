724206d599650463111f1ee9fcfc08ea3d0b1bbe
sierra.s <sierra.s@kakaocorp.com>
2021-01-05 15:23:00 +0900

    feat(Cars class) : 우승자를 판단하는 기능 구현
    getWinners : 우승한 차들을 List<Car>로 반환

--- src/main/java/carRace/Cars.java
+++ src/main/java/carRace/Cars.java
@@ -1,7 +1,9 @@
 package carRace;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
 
@@ -18,6 +20,14 @@ public class Cars {
         for (int i = 0; i < carList.size(); i++) {
             carList.get(i).judgeAndGo(numbers.get(i));
         }
+    }
+
+    public List<Car> getWinners(){
+        Collections.sort(carList);
+        Car maxCar = carList.get(0);
+        return carList.stream()
+                .filter(car -> car.compareTo(maxCar) == 0)
+                .collect(Collectors.toList());
     }
 
     public String toString(){
--- src/test/java/carRace/CarsTest.java
+++ src/test/java/carRace/CarsTest.java
@@ -25,4 +25,24 @@ public class CarsTest {
         String expectedString = "car1 : \ncar2 : \ncar3 : -\ncar4 : -\n";
         assertThat(testCars.toString()).isEqualTo(expectedString);
     }
+
+    @Test
+    public void getWinners(){
+        /* phase 1 (0, 0, 1, 1)*/
+        List<Integer> numbers1 = Arrays.asList(0, 3, 4, 9);
+        testCars.moveAllCars(numbers1);
+
+        /* phase 2 (0, 1, 1, 1)*/
+        List<Integer> numbers2 = Arrays.asList(0, 4, 3, 2);
+        testCars.moveAllCars(numbers2);
+
+        /* phase 3 (1, 1, 2, 2)*/
+        List<Integer> numbers3 = Arrays.asList(4, 3, 6, 9);
+        testCars.moveAllCars(numbers3);
+
+        List<Car> winners = testCars.getWinners();
+
+        String expectedString = "[car3 : --, car4 : --]";
+        assertThat(winners.toString()).isEqualTo(expectedString);
+    }
 }
