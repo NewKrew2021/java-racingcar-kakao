fd047754fe593f5c6d3de90d76066b3170dbd87a
danny.ho <danny.ho@kakaocorp.com>
2021-01-05 12:18:27 +0900

    feat: add car movement function and finding winner

--- src/main/java/NumberUtil.java
+++ src/main/java/NumberUtil.java
@@ -1,11 +1,8 @@
 import java.util.Random;
 
 public class NumberUtil {
-
     public static int createRandomNumber() {
-
         Random random = new Random();
-
         return random.nextInt(10);
     }
 }
