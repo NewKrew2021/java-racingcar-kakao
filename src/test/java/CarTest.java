77f650e98de201b94c1b32440d18815b48354606
jon.snow <jon.snow@kakaocorp.com>
2021-01-05 19:54:36 +0900

    test(CarTest): 자동차가 같은 위치에 있는지를 확인하는 메소드 테스트

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -5,6 +5,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CarTest {
     @Test
+    public void samePosition(){
+        Car car1 = new Car("jon", 1);
+        Car car2 = new Car("snow", 1);
+        assertTrue(car1.isEqualPosition(car2));
+    }
+    @Test
     public void testMove(){
         Car car1 = new Car("jon", 0);
         Car car2 = new Car("snow", 1);
