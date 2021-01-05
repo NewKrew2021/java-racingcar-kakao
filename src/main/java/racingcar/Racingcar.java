44db3e2d63b8de56f24ffe20ec10f0d5214e87d4
kuri <denver5636@naver.com>
2021-01-05 18:23:26 +0900

    [feat] 자동차 위치 설정

--- src/main/java/racingcar/Racingcar.java
+++ src/main/java/racingcar/Racingcar.java
@@ -1,17 +1,56 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Racingcar {
-    private List<String> carList;
-    
-    List<String> getCarName(String s) {
-        return this.carList = Arrays.asList(s.split(","));
+    private List<Car> carList;
+
+    /* 자동차 이름 설정 */
+    public boolean setCarList(){
+        String[] carNames = getCarNames(Race.carNames);
+        this.carList = new ArrayList<>();
+        addCarName(carNames);
+
+        return checkCarNames();
     }
 
-    boolean carNameCheck(){
-        return this.carList.size() <= 5;
+
+    void addCarName(String[] carNames) {
+        for (int i = 0; i < carNames.length; i++) {
+            this.carList.add(new Car(carNames[i], 0));
+        }
     }
+
+    String[] getCarNames(String carNames) {
+        return carNames.split(",");
+    }
+
+    boolean checkCarNames(){
+        return this.carList.stream().allMatch(car -> car.carNameLengthCheck());
+    }
+
+    /* 자동차의 위치 설정 */
+    public void setCarLoc(RandomNumber randomNumber){
+        this.carList.forEach(car -> {
+            car.setLoc(dirToDis(randomNumber.setDirection()));
+        });
+    }
+
+    private int dirToDis(CarStatus dir) {
+        if (dir == CarStatus.Go) return 1;
+
+        return 0;
+    }
+
+    List<Car> setCalLocTest(RandomNumber randomNumber){
+        this.carList.forEach(car -> {
+            car.setLoc(dirToDis(randomNumber.setDirection()));
+        });
+        return this.carList;
+    }
+
+
 }
--- src/test/java/racingcar/RacingcarTest.java
+++ src/test/java/racingcar/RacingcarTest.java
@@ -1,11 +1,13 @@
 package racingcar;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,23 +15,42 @@ import static org.assertj.core.api.Assertions.*;
 
 class RacingcarTest {
     private Racingcar racingcar;
+    private RandomNumber randomNumber;
 
     @BeforeEach
-    void setting() {
+    void set() {
         racingcar = new Racingcar();
+        randomNumber = new RandomNumber();
     }
 
     @Test
-    void getName() {
-        assertThat(racingcar.getCarName("pobi,crong,honux")).isEqualTo(Arrays.asList("pobi","crong","honux"));
+    void splitNameCheck() {
+        assertThat(racingcar.getCarNames("pobi,crong,honux")).isEqualTo(new String[]{"pobi","crong","honux"});
     }
 
     @Test
     void carNameCheck() {
-        racingcar.getCarName("pobi, crong, honux");
-        assertThat(racingcar.carNameCheck()).isEqualTo(true);
+        Race.carNames = "pobi,crong,honux";
+        assertThat(racingcar.setCarList()).isEqualTo(true);
     }
 
+    @Test
+    @DisplayName("1대의 자동차 방향 확인")
+    void carDirection() {
+        CarStatus dir = randomNumber.setDirection();
+        assertThat(dir).isInstanceOfAny(CarStatus.class);
+    }
+
+    @Test
+    @DisplayName("N대의 자동차 전진/멈춤")
+    void carMoveCheck() {
+        Race.carNames = "pobi,crong,honux";
+        racingcar.setCarList();
+        List<Car> car = racingcar.setCalLocTest(randomNumber);
+        for (int i = 0; i <car.size() ; i++) {
+            assertThat(car.get(i).getLoc()).isNotNegative();
+        }
+    }
 
 
 
