2dc86f94c96bc0026a567ddb13d423a9402db3cf
echo.ohce <echo.ohce@kakaocorp.com>
2021-01-05 16:21:23 +0900

    실행 결과 및 우승자 출력 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,13 +3,19 @@ package racingcar;
 import java.util.stream.IntStream;
 
 public class Car {
-
     private int position;
+    private String name;
+
     private final int MAX_VALUE = 9;
     private final int MIN_VALUE = 0;
 
-    public Car() {
+    public Car(String name) {
+        this.name = name;
         this.position = 0;
+    }
+
+    public String getName(){
+        return this.name;
     }
 
     public int getPosition(){
--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,6 +1,10 @@
 package racingcar;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Collection;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -8,19 +12,68 @@ public class Racing {
 
     public List<Car> cars;
 
-    public void startRacing(int carCount, int round) {
-        createCars(carCount);
+    private final int MAX_CAR_NAME_LENGTH = 5;
+
+    public void startRacing() {
+
+        List<String> names;
+        do{
+            names = splitNames(enterCarNames());
+        }while(!checkCarNames(names));
+
+        createCars(names);
+
+        int round = enterRound();
+
+        System.out.println("실행 결과");
         IntStream.range(0, round).forEach(i -> {
             racing();
-            printCurrentCarPositions();
+            printCurrentCarStatus();
+            System.out.println();
         });
+
+        printWinnerName();
     }
 
-    private void createCars(int carCount) {
+    private void printWinnerName() {
+        System.out.print(String.join(",", getWinnerName()));
+        System.out.println("가 최종 우승했습니다.");
+    }
+
+    public void createCars(List<String> names) {
         this.cars = new ArrayList<>();
-        for (int i = 0; i < carCount; i++) {
-            cars.add(new Car());
+
+        for (int i = 0; i < names.size(); i++) {
+            cars.add(new Car(names.get(i)));
         }
+    }
+
+    private String enterCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        return str;
+    }
+
+    private List<String> splitNames(String str) {
+        return new ArrayList<>(Arrays.asList(str.split(",")));
+    }
+
+    //리펙토링 필요(들여쓰기 문)
+    private boolean checkCarNames(List<String> names) {
+        for(String name: names){
+            if (name.length() > MAX_CAR_NAME_LENGTH) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int enterRound() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        Scanner sc = new Scanner(System.in);
+        int round = sc.nextInt();
+        return round;
     }
 
     private void racing(){
@@ -29,8 +82,17 @@ public class Racing {
         }
     }
 
-    private void printCurrentCarPositions() {
-        System.out.println("AAA");
+    private void printCurrentCarStatus() {
+        for(Car car: this.cars){
+            System.out.print(car.getName()+" : ");
+            printBars(car.getPosition());
+        }
+    }
+    private void printBars(int number){
+        IntStream.range(0, number).forEach(i ->{
+            System.out.print("-");
+        });
+        System.out.println();
     }
 
     public ArrayList<Integer> getFinalCarPositions() {
@@ -41,10 +103,24 @@ public class Racing {
         return positions;
     }
 
-    public ArrayList<Integer> getWinner() {
+    public ArrayList<Car> getWinner() {
         int maxPosition = max(getFinalCarPositions());
-        return getWinnerCarsIndex(maxPosition, getFinalCarPositions());
+
+        ArrayList<Car> winnerCars = new ArrayList<>();
+        for(int carInex: getWinnerCarsIndex(maxPosition, getFinalCarPositions())){
+            winnerCars.add(this.cars.get(carInex));
+        }
+        return winnerCars;
     }
+
+    private ArrayList<String> getWinnerName(){
+        ArrayList<String> names = new ArrayList<>();
+        for(Car car: getWinner()){
+            names.add(car.getName());
+        }
+        return names;
+    }
+
 
     private ArrayList<Integer> getWinnerCarsIndex(int maxPosition, List<Integer> currentCarPositions) {
         ArrayList<Integer> result = new ArrayList<>();
