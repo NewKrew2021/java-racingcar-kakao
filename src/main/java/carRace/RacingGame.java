256a3e2cf23dedd4dc0de8864f79f17727576063
sierra.s <sierra.s@kakaocorp.com>
2021-01-05 17:14:07 +0900

    feat(RacingGame class) : 게임 컨트롤러 작성
    run : 게임 전체를 진행한다.
    race : 반복하여 자동차를 경주시킨다.

---
+++ src/main/java/carRace/RacingGame.java
@@ -0,0 +1,18 @@
+package carRace;
+
+public class RacingGame {
+
+    public void run(){
+        Cars cars = RacingGameUI.getCarsFromUser();
+        race(RacingGameUI.getNumOfRepeatFromUser(), cars);
+        RacingGameUI.printWinners(cars.getWinners());
+    }
+
+    private void race(int n, Cars cars) {
+        RacingGameUI.printStartMessage();
+        for (int i = 0; i < n; i++) {
+            cars.moveAllCars(cars.getCarsRandomNumber());
+            RacingGameUI.printAllCars(cars);
+        }
+    }
+}
