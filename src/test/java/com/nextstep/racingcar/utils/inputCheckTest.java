426535f8d205363c76dfdcc71a2ece3635c189fb
Jjungs7 <jeongy95@gmail.com>
2021-01-05 17:30:10 +0900

    refactor(inputcheck): change repeating code
    Change to parameterized test to remove one portion of repeating code

--- src/test/java/com/nextstep/racingcar/utils/inputCheckTest.java
+++ src/test/java/com/nextstep/racingcar/utils/inputCheckTest.java
@@ -1,21 +1,20 @@
 package com.nextstep.racingcar.utils;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 public class inputCheckTest {
 
-  @Test
-  void invalidCarName() {
+  @ParameterizedTest
+  @ValueSource(strings = {"longname", "na!me", "nam3"})
+  void invalidCarName(String name) {
     assertThatExceptionOfType(RuntimeException.class).
         isThrownBy(
-            () -> InputCheck.isValidName("longname"));
-
-    assertThatExceptionOfType(RuntimeException.class).
-        isThrownBy(
-            () -> InputCheck.isValidName("na!me"));
+            () -> InputCheck.isValidName(name));
   }
 
   @Test
