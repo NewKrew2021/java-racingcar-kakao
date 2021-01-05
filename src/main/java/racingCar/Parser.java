bc5adc003e5b2188b32a72930abeba09ad1a6cce
kth496 <taehongkim496@gmail.com>
2021-01-05 11:13:19 +0900

    feat: 글자개수가 5개가 넘어갈때 runtimeException 발생기능 추가

--- src/main/java/racingCar/Parser.java
+++ src/main/java/racingCar/Parser.java
@@ -9,10 +9,21 @@ public class Parser {
     public Parser() {
     }
 
-    public List<String> parsing(String userInput) {
+    public List<String> parsing(String userInput) throws RuntimeException {
         String[] split = userInput.split(",");
         return Arrays.stream(split)
                      .map(String::trim)
+                     .map(this::isLong)
                      .collect(Collectors.toList());
     }
+
+    public String isLong(String carName){
+
+        if(carName.length()>5){
+            throw new RuntimeException("이름이 너무 깁니다");
+        }
+        return carName;
+    }
+
+
 }
--- src/test/java/racingCar/racingTest.java
+++ src/test/java/racingCar/racingTest.java
@@ -1,13 +1,22 @@
 package racingCar;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
 public class racingTest {
+
+    private Parser parser;
+    @BeforeEach
+    public void setUp(){
+        parser=new Parser();
+    }
     /*
     1. 자동차 이름 문자열은 ,로 구분된다.
     , 앞뒤의 공백은 자동으로 지워준다.
@@ -15,9 +24,14 @@ public class racingTest {
     @Test
     public void carNameTest() {
         String userInput = "pobi,crong,honux, teo ";
-        Parser parser = new Parser();
         List<String>  result = parser.parsing(userInput);
         Assertions.assertThat(result)
                     .containsAll(Arrays.asList("pobi", "crong", "honux", "teo"));
+    }
+
+    @Test
+    public void longNameExceptionTest() {
+        String userInput = "bmoadv,pobi,vavaa";
+        assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> parser.parsing(userInput));
     }
 }
