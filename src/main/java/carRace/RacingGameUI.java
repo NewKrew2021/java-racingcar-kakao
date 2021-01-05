6b42c2a14c240e957936f6052aa7bf383993844d
dy.namic <dy.namic@kakaocorp.com>
2021-01-05 23:12:30 +0900

    fix(RacingGameUI class) : 사용자가 차 이름 입력할때 space 처리 못하는 버그 수정
    getCarsFromUser 메소드에서 사용자 입력 받는 부분을 scanner.next() 에서 scanner.nextLine()으로 변경

--- src/main/java/carRace/RacingGameUI.java
+++ src/main/java/carRace/RacingGameUI.java
@@ -11,7 +11,7 @@ public class RacingGameUI {
     public static Cars getCarsFromUser() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String input = scanner.next();
+        String input = scanner.nextLine();
         return validationAndConvert(split(input));
     }
 
