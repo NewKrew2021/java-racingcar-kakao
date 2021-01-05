43e20bafa4b91113dc709676c3ad47646b27a5ae
kth496 <taehongkim496@gmail.com>
2021-01-05 11:24:55 +0900

    feat: 특정 랜덤 값을 기준으로 자동차 전진, 정지 기능 구현

--- src/main/java/racingCar/Car.java
+++ src/main/java/racingCar/Car.java
@@ -3,9 +3,18 @@ package racingCar;
 public class Car {
 
     private final String name;
+    private Integer position;
 
     public Car(String name) {
         this.name = name;
+        this.position = 0;
     }
 
+    public void goForward(Integer value) {
+        if (value >= 4) this.position++;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
 }
--- src/test/java/racingCar/racingTest.java
+++ src/test/java/racingCar/racingTest.java
@@ -8,8 +8,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-
 public class racingTest {
 
     private Parser parser;
