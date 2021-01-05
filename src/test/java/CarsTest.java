50d33889d935b76aa091f746ff4a5c1747c38633
jon.snow <jon.snow@kakaocorp.com>
2021-01-05 20:20:23 +0900

    test(CarsTest): 선두 그룹을 알맞게 리턴하는지를 테스트

--- src/test/java/CarsTest.java
+++ src/test/java/CarsTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CarsTest {
@@ -7,5 +10,27 @@ public class CarsTest {
     public void countTest(){
         Cars cars = new Cars("jon,snow,danial");
         assertEquals(cars.getCount(), 3);
+    }
+
+    @Test
+    public void checkHeads(){
+        List<Car> list = new ArrayList<>();
+        Car car1 = new Car("jon",1);
+        Car car2 = new Car("snow", 2);
+        Car car3 = new Car("danial",3);
+
+        list.add(
+                car1
+        );
+        list.add(
+                car2
+        );
+        list.add(
+                car3
+        );
+
+        Cars cars = new Cars(list);
+        List<Car> heads = cars.getHeads();
+        assertTrue(heads.get(0).equals(car3));
     }
 }
