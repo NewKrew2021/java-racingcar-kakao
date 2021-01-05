8eea38ad0375cb9e0104855d3bd06692152ce170
jon.snow <jon.snow@kakaocorp.com>
2021-01-05 20:02:55 +0900

    test(CarsTest): 이름들을 입력받아 알맞은 car 리스트를 생성하는지를 테스트

---
+++ src/test/java/CarsTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CarsTest {
+    @Test
+    @Test
+    public void countTest(){
+        Cars cars = new Cars("jon,snow,danial");
+        assertEquals(cars.getCount(), 3);
+    }
+}
