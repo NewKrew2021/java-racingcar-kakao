1b853d7924c93f2fbf5a0c3f80df8625bc4e762d
jh280722 <jh280722@naver.com>
2021-01-05 12:53:08 +0900

    feat($RacingCarGameApp) : add scan method and print method
    - add printWinners method
    - add getText method
    - add getRacingNum method

--- src/main/java/racingcar/RacingCarGameApp.java
+++ src/main/java/racingcar/RacingCarGameApp.java
@@ -4,14 +4,36 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class RacingCarGameApp {
+    private static final Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         RacingCarGame game = new RacingCarGame();
-        Scanner sc = new Scanner(System.in);
 
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String text = sc.nextLine();
-        int racingNum = sc.nextInt();
+        String text = getText();
+        int racingNum = getRacingNum();
 
         game.start(text, racingNum);
+
+        printWinners(game);
+    }
+
+    private static int getRacingNum() {
+        System.out.println("시도할 회수는 몇회인가?");
+        return sc.nextInt();
+    }
+
+    private static String getText() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return sc.nextLine();
+    }
+
+    private static void printWinners(RacingCarGame game) {
+        StringBuilder stb = new StringBuilder();
+
+        ArrayList<String> winners = game.getWinners();
+        for (String winner : winners) {
+            stb.append(winner).append(",");
+        }
+        System.out.print(stb.substring(0,stb.length() - 1));
+        System.out.println("가 최종 우승했습니다.");
     }
 }
--- src/test/java/racingcar/RacingCarGameTest.java
+++ src/test/java/racingcar/RacingCarGameTest.java
@@ -1,6 +1,5 @@
 package racingcar;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
