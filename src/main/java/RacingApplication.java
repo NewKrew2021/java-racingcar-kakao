18572d32ccc6c5a49198664cdbc3bb1e9debb5fc
corby-kim <corby.kim@kakaocorp.com>
2021-01-06 11:15:43 +0900

    refactor: rename main class

--- src/main/java/RacingApplication.java
+++ src/main/java/RacingApplication.java
@@ -3,7 +3,7 @@ import domain.RandomEngine;
 import view.InputView;
 import view.OutputView;
 
-public class RacingCarGame {
+public class RacingApplication {
     public static void main(String[] args) {
         RacingGame game = RacingGame.initialize(new RandomEngine(), InputView.readNames(), InputView.readRound());
         while (!game.isEnd()) {
