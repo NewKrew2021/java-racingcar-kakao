e8f45a71aba76e3a8c4c406b640de30304ca791d
tttuer <tttuer1@naver.com>
2021-01-06 18:51:43 +0900

    feat: update Main
    update Main

--- src/main/java/racingcar/Main.java
+++ src/main/java/racingcar/Main.java
@@ -8,14 +8,12 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        GameManager gameManager = new GameManager();
         Output.askCarsName();
         List<String> carsName = Input.getNames();
-        gameManager.createCars(carsName);
 
         Output.askGameTime();
         int gameTime = Input.getGameTime();
-        gameManager.setGameTime(gameTime);
+        GameManager gameManager = new GameManager(carsName, gameTime);
 
         gameManager.playGame();
     }
