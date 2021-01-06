99c8d1056f5fd7e6b0ccabf53453502d40f10622
jack.won <jack.won@kakaocorp.com>
2021-01-06 17:18:37 +0900

    feat(randomnumbergenerator): 랜덤 숫자 생성기에 검증기능 추가
    * car에 제공되는 랜덤 숫자에 이상이 없도록 확고히 한다

--- src/main/java/racingcar/utils/RandomNumberGenerator.java
+++ src/main/java/racingcar/utils/RandomNumberGenerator.java
@@ -1,9 +1,25 @@
 package racingcar.utils;
 
+import racingcar.io.Text;
+
 public class RandomNumberGenerator {
     private static final int MAX = 10;
+    private static final int MIN_RANDOM_NUM = 0;
+    private static final int MAX_RANDOM_NUM = 9;
 
     public int getRandomNumber() {
-        return (int)(Math.random() * MAX);
+        int random = (int)(Math.random() * MAX);
+        validateRandomNumber(random);
+        return random;
+    }
+
+    private void validateRandomNumber(int random){
+        if(isInvalid(random)){
+            throw new IllegalArgumentException(Text.ILLEGAL_RANDOM_NUMBER);
+        }
+    }
+
+    private boolean isInvalid(int random) {
+        return random > MAX_RANDOM_NUM || random < MIN_RANDOM_NUM;
     }
 }
