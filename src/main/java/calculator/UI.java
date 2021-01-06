4b8297bbedc42d0cb80a5c1e140a35e688f4c09a
tttuer <tttuer1@naver.com>
2021-01-06 20:29:01 +0900

    feat: update calculator/UI
    update calculator/UI

--- src/main/java/calculator/UI.java
+++ src/main/java/calculator/UI.java
@@ -5,13 +5,12 @@ import java.util.Scanner;
 public class UI {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Input input = null;
         String in = "";
 
         System.out.println("계산할 문자 입력 >> ");
         in = scanner.next();
 
-        input = new Input(in);
+        Input input = new Input(in);
         System.out.println("결과 >> " + input.add());
     }
 }
