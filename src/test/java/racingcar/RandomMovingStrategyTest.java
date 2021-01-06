a1421ff43b227e3a9fc059f382ef725c6c43eba7
brody <youoper@gmail.com>
2021-01-06 22:57:30 +0900

    test(RandomMovingStrategyTest): 난수값을 범위 내에서 생성하고, 정해진 조건에 따라 기준을 판단하는지 확인하는 테스트 추가

---
+++ src/test/java/racingcar/RandomMovingStrategyTest.java
@@ -0,0 +1,26 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RandomMovingStrategyTest {
+
+    private final RandomMovingStrategy randomMovingStrategy = new RandomMovingStrategy();
+
+    @Test
+    @DisplayName("난수값이 0과 9 사이에 있는지 확인한다.")
+    void makeRandomNumberTest(){
+        assertThat(randomMovingStrategy.makeRandomNumber()).isGreaterThan(-1).isLessThan(10);
+    }
+
+    @Test
+    @DisplayName("난수값이 3이하인 경우에는 기준을 넘지 않고, 4이상이면 기준을 넘는지 확인한다.")
+    void exceedMovingCriterionTest(){
+        int stopRandomNumber = 3;
+        int goRandomNumber = 4;
+        assertThat(randomMovingStrategy.exceedMovingCriterion(stopRandomNumber)).isFalse();
+        assertThat(randomMovingStrategy.exceedMovingCriterion(goRandomNumber)).isTrue();
+    }
+}
