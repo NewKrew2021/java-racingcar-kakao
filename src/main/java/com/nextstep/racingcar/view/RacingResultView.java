4ef1c5525f588489bf44c9d18cfc0c7f5fd2b7bd
sumniy <sumniy94@gmail.com>
2021-01-06 17:34:18 +0900

    Refactor: RacingcarView 클래스를 입력, 출력을 담당하는 클래스로 분리
    RacingInputView와 RacingResultView로 분리하여
    클래스의 역할을 더 작게 분리함.
    출력에 대한 책임을 Car나 Cars 객체가 가지지 않도록
    각 클래스에서 toString을 삭제하고, RacingResultView 클래스가 그리도록 변경함
    
    구현 내용
    - RacingcarView를 입력, 출력을 담당하는 2개의 클래스로 분리
    - 경기 결과를 그리는 책임을 View 클래스가 담당하도록 책임 이동

---
+++ src/main/java/com/nextstep/racingcar/view/RacingResultView.java
@@ -0,0 +1,28 @@
+package com.nextstep.racingcar.view;
+
+import com.nextstep.racingcar.domain.Car;
+import com.nextstep.racingcar.domain.Cars;
+
+public class RacingResultView {
+
+  private RacingResultView() {}
+
+  public static void printRaceResultMessage() {
+    System.out.println("경기 결과");
+  }
+
+  public static void printCar(Car car) {
+    System.out.println(String.format("%s : %s", car.getName(), "-".repeat(car.getLocation())));
+  }
+
+  public static void printCars(Cars cars) {
+    for (Car car: cars.getCars()) {
+      printCar(car);
+    }
+    System.out.println();
+  }
+
+  public static void printWinners(Cars cars) {
+    System.out.println(String.join(", ", cars.getWinners()) + "가 최종 우승했습니다.");
+  }
+}
