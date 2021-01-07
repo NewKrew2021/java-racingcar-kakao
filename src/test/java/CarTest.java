ad61f0b0ae55d033f39dc2484530ae2ddfd5f6d6
jon.snow <jon.snow@kakaocorp.com>
2021-01-07 11:11:04 +0900

    refactor(CarTest) : 테스트 케이스를 경계값 위주로 변경

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,19 +1,19 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarTest {
     @Test
-    public void samePosition(){
+    public void samePosition() {
         Car car1 = new Car("jon", 1);
         Car car2 = new Car("snow", 1);
         assertTrue(car1.isEqualPosition(car2));
     }
     @Test
-    public void testMove(){
-        Car car1 = new Car("jon", 0);
-        Car car2 = new Car("snow", 1);
+    public void testMove() {
+        Car car1 = new Car("jon", 4);
+        Car car2 = new Car("snow", 5);
         car1.move(5);
         assertTrue(car1.isEqualPosition(car2));
     }
