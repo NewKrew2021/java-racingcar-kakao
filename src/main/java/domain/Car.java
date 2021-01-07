eff49a0141bd155de04a462605f0f8af685a4eb2
Sangyeol Kim <syeol.kim@daum.net>
2021-01-07 12:00:43 +0900

    refactor: add constructor overloading

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,15 +1,12 @@
 package domain;
 
 public class Car {
-//    public final static Comparator<Car> comparotr = new Comparator<Car>() {
-//        @Override
-//        public int compare(Car o1, Car o2) {
-//            return Integer.compare(o1.getPosition(), o2.getPosition());
-//        }
-//    };
-
     private final String name;
     private int position;
+
+    public Car(String name) {
+        this(name, 0);
+    }
 
     public Car(String name, int position) {
         this.name = name;
