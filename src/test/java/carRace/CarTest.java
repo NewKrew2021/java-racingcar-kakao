69c593bfa27868e52e262f47c5ccb40c8d9a13e8
sierra.s <sierra.s@kakaocorp.com>
2021-01-07 19:14:46 +0900

    feat: compareTo 테스트 케이스 추가
    값이 작은 경우, 큰 경우, 같은 경우에 대해 모두 테스트하도록 추가

--- src/test/java/carRace/CarTest.java
+++ src/test/java/carRace/CarTest.java
@@ -1,26 +1,47 @@
 package carRace;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
 
-    @Test
-    public void goTest() {
-        Car testCar = new Car("test");
+    Car testCar;
+
+    @BeforeEach
+    public void setUp() {
+        testCar = new Car("test");
         testCar.go(() -> true);
-        testCar.go(() -> true);
-        assertThat(testCar.toString()).isEqualTo("test : --");
     }
 
     @Test
-    public void compareToTest() {
-        Car testCar1 = new Car("test1");
-        Car testCar2 = new Car("test2");
-        testCar1.go(() -> true);
-        testCar2.go(() -> true);
-        testCar2.go(() -> true);
-        assertThat(testCar1.compareTo(testCar2)).isLessThan(0);
+    public void goTest() {
+        assertThat(testCar.toString()).isEqualTo("test : -");
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideCar")
+    public void compareToTest(Car otherCar, int expected) {
+        assertThat(testCar.compareTo(otherCar)).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideCar() {
+        Car car1 = new Car("test1");
+        Car car2 = new Car("test2");
+        Car car3 = new Car("test3");
+        car1.go(() -> true);
+        car1.go(() -> true);
+        car3.go(() -> true);
+        return Stream.of(
+                Arguments.of(car1, -1),
+                Arguments.of(car2, 1),
+                Arguments.of(car3, 0)
+        );
     }
 }
