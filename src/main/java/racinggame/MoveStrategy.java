2127800abd4af85909d4e723741dd1423474b6a2
danial.yoo <danial.yoo@kakaocorp.com>
2021-01-07 19:11:54 +0900

    refactor - 패키지 구조 변경

--- src/main/java/racinggame/MoveStrategy.java
+++
@@ -1,6 +0,0 @@
-package racinggame;
-
-@FunctionalInterface
-public interface MoveStrategy {
-    public boolean isMove();
-}
--- src/main/java/racinggame/UserInputValidator.java
+++
@@ -1,5 +0,0 @@
-package racinggame;
-
-public interface UserInputValidator {
-    public void validate(String str) throws Exception;
-}
