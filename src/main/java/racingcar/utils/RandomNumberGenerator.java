4969588382cda709a64a00c7cbdcd02f526ac5d1
jack.won <jack.won@kakaocorp.com>
2021-01-07 18:39:36 +0900

    refactor(randomnumbergenerator): 메서드들을 static으로 변경
    * 인스턴스 생성이 필요 없는 클래스

--- src/main/java/racingcar/utils/RandomNumberGenerator.java
+++ src/main/java/racingcar/utils/RandomNumberGenerator.java
@@ -7,19 +7,19 @@ public class RandomNumberGenerator {
     private static final int MIN_RANDOM_NUM = 0;
     private static final int MAX_RANDOM_NUM = 9;
 
-    public int getRandomNumber() {
-        int random = (int)(Math.random() * MAX);
+    public static int getRandomNumber() {
+        int random = (int) (Math.random() * MAX);
         validateRandomNumber(random);
         return random;
     }
 
-    private void validateRandomNumber(int random){
-        if(isInvalid(random)){
+    private static void validateRandomNumber(int random) {
+        if (isInvalid(random)) {
             throw new IllegalArgumentException(Text.ILLEGAL_RANDOM_NUMBER);
         }
     }
 
-    private boolean isInvalid(int random) {
+    private static boolean isInvalid(int random) {
         return random > MAX_RANDOM_NUM || random < MIN_RANDOM_NUM;
     }
 }
