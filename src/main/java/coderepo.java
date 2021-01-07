ca8d2ee4cb936b70b0e57f2f59c6ac98ea7599aa
jon.snow <jon.snow@kakaocorp.com>
2021-01-07 13:10:15 +0900

    refactor(Main) : Main에서 IOException을 받도록 수정

---
+++ src/main/java/coderepo.java
@@ -0,0 +1,25 @@
+public class coderepo {
+}
+
+
+/*
+
+
+    public static void print(String input){
+        System.out.print(input);
+    }
+
+    public static void println(String input){
+        System.out.println(input);
+    }
+
+
+
+    @Test
+    public void testValidName(){
+        assertThatThrownBy(() -> {
+            Car car = new Car("666666", 0);
+        });
+    }
+
+ */
