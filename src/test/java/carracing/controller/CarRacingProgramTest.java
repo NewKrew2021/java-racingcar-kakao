ba7e8833b792e9f3021ee70db07796beffc8ca45
pharrell.mp3 <pharrell.mp3@kakaocorp.com>
2021-01-08 16:06:44 +0900

    refactor(test) : displayName을 사용하여 메소드 설명 작성
    - 메소드 명에 테스트의 의미를 부여하지 않고 displayName annotation을 통하여 설명

---
+++ src/test/java/carracing/controller/CarRacingProgramTest.java
@@ -0,0 +1,23 @@
+package carracing.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarRacingProgramTest {
+
+    private CarRacingProgram carRacingProgram;
+
+    @BeforeEach
+    void setUp() {
+        carRacingProgram = new CarRacingProgram();
+    }
+
+    @Test
+    @DisplayName("랜덤 값이 범위 내에 있는지 여부 테스트")
+    void getRandomNumber() {
+        assertThat(carRacingProgram.getRandomNumber()).isBetween(0, 9);
+    }
+}
--- src/test/java/carracing/domain/CarTest.java
+++ src/test/java/carracing/domain/CarTest.java
@@ -1,5 +1,6 @@
 package carracing.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,18 +9,21 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class CarTest {
 
     @Test
+    @DisplayName("차의 객체 생성 테스트")
     void generateCar() {
         Car car = new Car("car1");
         assertThat(car.getCarInfoString()).isEqualTo("car1 : ");
     }
 
     @Test
-    void goTest() {
+    @DisplayName("차의 위치를 보여주는 문자열 비교 테스트")
+    void getCarInfoString() {
         Car car = new Car("car1", 2);
         assertThat(car.getCarInfoString()).isEqualTo("car1 : --");
     }
 
     @Test
+    @DisplayName("차의 이름 설정에 대한 유효성 테스트")
     void isValidNameFormat() {
         assertThatThrownBy(() -> new Car(null)).isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> new Car("")).isInstanceOf(IllegalArgumentException.class);
@@ -27,14 +31,16 @@ public class CarTest {
     }
 
     @Test
-    void isPositionEqualMaxMoveDistanceTest_equal() {
+    @DisplayName("차의 위치와 파라미터값이 같은지 판단하는 기능 테스트 - 위치가 같을 때")
+    void isCarPositionEqualToIndex1() {
         int maxMoveDistance = 2;
         Car car = new Car("car1", 2);
         assertThat(car.isCarPositionEqualToIndex(maxMoveDistance)).isTrue();
     }
 
     @Test
-    void isPositionEqualMaxMoveDistanceTest_unequal() {
+    @DisplayName("차의 위치와 파라미터값이 같은지 판단하는 기능 테스트 - 위치가 다를 때")
+    void isCarPositionEqualToIndex2() {
         int maxMoveDistance = 2;
         Car car = new Car("car1", 1);
         assertThat(car.isCarPositionEqualToIndex(maxMoveDistance)).isFalse();
