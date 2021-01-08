c45602cb280a40aabd6bcbbccc6ebed470bb2295
jon.snow <jon.snow@kakaocorp.com>
2021-01-08 10:12:00 +0900

    refactor(CarsTest) : 테스트 케이스 알맞게 수정

--- src/test/java/CarsTest.java
+++ src/test/java/CarsTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CarsTest {
     @Test
     public void countTest(){
-        Cars cars = new Cars("jon,snow,danial");
+        Cars cars = new Cars("jon,snow,dania");
         assertEquals(cars.getCount(), 3);
     }
 
@@ -17,17 +17,11 @@ public class CarsTest {
         List<Car> list = new ArrayList<>();
         Car car1 = new Car("jon",1);
         Car car2 = new Car("snow", 2);
-        Car car3 = new Car("danial",3);
+        Car car3 = new Car("dania",3);
 
-        list.add(
-                car1
-        );
-        list.add(
-                car2
-        );
-        list.add(
-                car3
-        );
+        list.add(car1);
+        list.add(car2);
+        list.add(car3);
 
         Cars cars = new Cars(list);
         List<Car> heads = cars.getHeads();
