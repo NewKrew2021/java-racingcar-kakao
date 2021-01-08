eb3435b363f8d6682dcba4d1fa98c64c9db8a43a
pharrell.mp3 <pharrell.mp3@kakaocorp.com>
2021-01-08 13:56:01 +0900

    refactor(view) : phrase 클래스에 정의된 문자열 상수를 view(UI)클래스로 이관
    - 상수만 정의된 클래스 제거 및 상수를 실제 사용하는 클래스로 이관

--- src/main/java/carracing/ui/CarRacingProgramUI.java
+++ src/main/java/carracing/ui/CarRacingProgramUI.java
@@ -1,43 +1,47 @@
 package carracing.ui;
 
 import carracing.domain.Car;
-import carracing.utils.CarRacingProgramPhrases;
 
 import java.util.List;
 
 public class CarRacingProgramUI {
 
-   public static void printCarInputNamesPharse(){
-       System.out.println(CarRacingProgramPhrases.CAR_NAMES_INPUT_PHRASE);
-   }
+    public static final String CAR_NAMES_INPUT_PHRASE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    public static final String RACE_COUNT_PHRASE = "시도할 회수는 몇회인가요?";
+    public static final String RESULT_PHRASE = "실행 결과";
+    public static final String WINNER_PHRASE = "가 최종 우승했습니다.";
 
-    public static void printRaceCountPhrase(){
-        System.out.println(CarRacingProgramPhrases.RACE_COUNT_PHRASE);
+    public static void printCarInputNamesPharse() {
+        System.out.println(CAR_NAMES_INPUT_PHRASE);
     }
 
-    public static void printResultPharse(){
-        System.out.println(CarRacingProgramPhrases.RESULT_PHRASE);
+    public static void printRaceCountPhrase() {
+        System.out.println(RACE_COUNT_PHRASE);
     }
 
-    public static void printWinnerPharse(List<String> winners){
+    public static void printResultPharse() {
+        System.out.println(RESULT_PHRASE);
+    }
+
+    public static void printWinnerPharse(List<String> winners) {
         StringBuilder winnerBuilder = new StringBuilder();
 
-        for (int i = 0; i < winners.size()-1; i++) {
+        for (int i = 0; i < winners.size() - 1; i++) {
             winnerBuilder.append(winners.get(i));
             winnerBuilder.append(", ");
         }
-        winnerBuilder.append(winners.get(winners.size()-1));
+        winnerBuilder.append(winners.get(winners.size() - 1));
 
-        winnerBuilder.append(CarRacingProgramPhrases.WINNER_PHRASE);
+        winnerBuilder.append(WINNER_PHRASE);
 
         System.out.println(winnerBuilder.toString());
     }
 
-    public static void printCarPosition(Car car){
+    public static void printCarPosition(Car car) {
         System.out.println(car.getCarInfoString());
     }
 
-    public static void printBlankLine(){
+    public static void printBlankLine() {
         System.out.println();
     }
 }
