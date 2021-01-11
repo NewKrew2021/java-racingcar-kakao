da0272385219791fe43bd7515b1179fede1d4a6e
dy.namic <dy.namic@kakaocorp.com>
2021-01-12 00:59:04 +0900

    test : StringUtilTest를 ParameterizedTest로 변경

--- src/test/java/carRace/utils/StringUtilTest.java
+++ src/test/java/carRace/utils/StringUtilTest.java
@@ -1,32 +1,26 @@
 package carRace.utils;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringUtilTest {
 
     @Test
-    public void isAlphabetString_Test_1() {
+    public void isAlphabetString_Test_true() {
         String testString = "abcdefg";
         assertThat(StringUtil.isAlphabetString(testString)).isEqualTo(true);
     }
 
-    @Test
-    public void isAlphabetString_Test_2() {
-        String testString = "abcdefg h";
-        assertThat(StringUtil.isAlphabetString(testString)).isEqualTo(false);
-    }
-
-    @Test
-    public void isAlphabetString_Test_3() {
-        String testString = "ab3defg";
-        assertThat(StringUtil.isAlphabetString(testString)).isEqualTo(false);
-    }
-
-    @Test
-    public void isAlphabetString_Test_4() {
-        String testString = " !bcdefg";
+    @ParameterizedTest
+    @ValueSource(strings = {"abcdefg h", "ab3defg", "  ", " !bcdefg"})
+    public void isAlphabetString_Test_false(String testString) {
         assertThat(StringUtil.isAlphabetString(testString)).isEqualTo(false);
     }
 }
